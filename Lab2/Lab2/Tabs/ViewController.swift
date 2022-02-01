6074bab54b7c03d81263d73bf96af626dd0b48a1
eurekesh <asdu8905@colorado.edu>
2022-01-31 23:09:08 -0700

    made picker dependent

--- Lab2/Lab2/Tabs/ViewController.swift
+++ Lab2/Lab2/Tabs/ViewController.swift
@@ -10,39 +10,72 @@ import UIKit
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet weak var releaseDateLabel: UILabel!
+    let categories = ["Video Games", "Board Games"]
+    var activeReleases = [String]()
+    var activeReleaseDates = [Date]()
     
-    var releases = [String]()
-    var releaseDates = [Date]()
+    var vgReleases = [String]()
+    var vgDates = [Date]()
+    var boardReleases = [String]()
+    var boardDates = [Date]()
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
+        return 2
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return releases.count
+        if component == 0 {
+            return categories.count
+        } else {
+            return activeReleases.count
+        }
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return releases[row]
+        if (component == 0) {
+            return categories[row]
+        }
+        return activeReleases[row]
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        setDateText(row);
+        if (component == 0) {
+            if (row == 0) {
+                activeReleases = vgReleases
+                activeReleaseDates = vgDates
+                pickerView.reloadComponent(1)
+                pickerView.selectRow(0, inComponent: 1, animated: true)
+            } else {
+                activeReleases = boardReleases
+                activeReleaseDates = boardDates
+                pickerView.reloadComponent(1)
+                pickerView.selectRow(0, inComponent: 1, animated: true)
+            }
+            setDateText(0)
+        }
+        
+        if (component == 1) {
+            setDateText(row);
+        }
     }
     
     private func setDateText(_ row: Int) {
         // help from https://stackoverflow.com/questions/35700281/date-format-in-swift
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MM-dd-yyyy HH:mm"
-        releaseDateLabel.text="\(releases[row]) releases on \(dateFormatter.string(from: releaseDates[row]))"
+        releaseDateLabel.text="\(activeReleases[row]) releases on \(dateFormatter.string(from: activeReleaseDates[row]))"
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let loader = Loader()
         loader.load()
-        releases = loader.getReleases()
-        releaseDates = loader.getReleaseDates()
+        vgReleases = Array(loader.getReleases().prefix(4))
+        vgDates = Array(loader.getReleaseDates().prefix(4))
+        boardReleases = Array(loader.getReleases().suffix(2))
+        boardDates = Array(loader.getReleaseDates().suffix(2))
+        activeReleases = vgReleases
+        activeReleaseDates = vgDates
         setDateText(0)
     }
 
